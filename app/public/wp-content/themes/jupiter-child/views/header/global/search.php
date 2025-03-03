9f866d5026f6fced116a8def50119828f4d4edc7
Laureline Auger <laureline.auger@gmail.com>
2025-03-03 14:29:33 +0100

    initial commit

---
+++ app/public/wp-content/themes/jupiter-child/views/header/global/search.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * template part for search in header and header toolbar. views/header/global
+ *
+ * @author 		Artbees
+ * @package 	jupiter/views
+ * @version     5.0.0
+ */
+
+global $mk_options;
+
+if ($mk_options['header_search_location'] != $view_params['location']) return false; ?>
+
+<div class="mk-header-search">
+    <a class="menu-item-link js-smooth-scroll" target="_blank" href="https://www.hypnose-sophrologie-paris.com/contact/">Prendre rendez-vous</a>
+</div>
